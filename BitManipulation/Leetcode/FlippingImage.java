e27392d1f20e9b06707f37bf2e385ed223a6aa41
Akram <akramshaik2910@gmail.com>
2026-08-07 15:45:44 +0530

    Solve leetcode flip image problem

---
+++ BitManipulation/Leetcode/FlippingImage.java
@@ -0,0 +1,36 @@
+package Leetcode;
+
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        int r = image.length;
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0, k = r - 1; j <= k; j++, k--) {
+                if (j == k) {
+                    image[i][j] ^= 1;
+                    break;
+                }
+                swap(image, i, j, k);
+                flip(image, i, j, k);
+            }
+        }
+        return image;
+    }
+
+    private void flip(int[][] image, int i, int j, int k) {
+        image[i][j] ^= 1;
+        image[i][k] ^= 1;
+    }
+
+    public static void swap(int[][] image, int i, int j, int k) {
+        int temp = image[i][j];
+        image[i][j] = image[i][k];
+        image[i][k] = temp;
+    }
+}
+
+public class FlippingImage {
+    public static void main(String[] args) {
+
+    }
+}

f895ff89cca5a0e455c258810fa2f4340ff5b3cc
Akram <akramshaik2910@gmail.com>
2026-08-08 10:11:50 +0530

    Solve Seive of Eratosthenes

---
+++ Mathematics/SeiveOfEratosthenes.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class SeiveOfEratosthenes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+
+        boolean[] primes = new boolean[n+1];
+
+        seive(n, primes);
+        
+        for(int i = 2; i <= n; i++) {
+            if(!primes[i]) {
+                System.out.println(i + " ");
+            }
+        }
+        sc.close();
+    }
+
+    private static void seive(int n, boolean[] primes) {
+        for(int i = 2; i*i <= n; i++){
+            if(!primes[i]) {
+                for(int j = i * 2; j <= n; j += i) {
+                    primes[j] = true;
+                }
+            }
+        }
+    }
+}

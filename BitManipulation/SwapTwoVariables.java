9ff2b19d2511d42314a016175c492fc31fa534b0
Akram <akramshaik2910@gmail.com>
2026-08-09 15:00:37 +0530

    Solve Swap 2 variables "

---
+++ BitManipulation/SwapTwoVariables.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class SwapTwoVariables {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a : ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter b : ");
+        int b = sc.nextInt();
+
+        System.out.printf("Before swap a = %d  b = %d\n",a, b);
+
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+
+        System.out.printf("After swap a = %d  b = %d",a, b);
+
+        sc.close();
+    }
+}

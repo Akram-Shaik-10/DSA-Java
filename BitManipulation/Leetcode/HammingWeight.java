195862095b7427861fdc01c2f50852adbc718ace
Akram <akramshaik2910@gmail.com>
2026-08-09 15:00:00 +0530

    Add Hamming weight problem

---
+++ BitManipulation/Leetcode/HammingWeight.java
@@ -0,0 +1,19 @@
+package Leetcode;
+//leetcode problem 191
+
+class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+        while(n > 0) {
+            if((n & 1) == 1) count++;
+            n = n >> 1;
+        }
+        return count;
+    }
+}
+
+public class HammingWeight {
+    public static void main(String[] args) {
+        
+    }
+}

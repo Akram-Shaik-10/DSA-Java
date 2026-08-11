d71cb59df1defbf1b5d1a10a4c74cdb0ab5676fd
Akram <akramshaik2910@gmail.com>
2026-08-11 12:45:46 +0530

    Solve Buy and Sell Stocks

---
+++ Array/Leetcode/BuyAndSellStocks.java
@@ -0,0 +1,26 @@
+//leetcode 121
+//Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+package Array.Leetcode;
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy = prices[0];
+        int profit = 0;
+        int sell = buy;
+        for(int i = 1; i < prices.length; i++) {
+            if(prices[i] < buy){
+                buy = prices[i];
+            }
+            sell = prices[i];
+            if(profit < sell - buy) profit = sell - buy;
+        }
+        return profit;
+    }
+}
+
+public class BuyAndSellStocks {
+    public static void main(String[] args) {
+        
+    }
+}

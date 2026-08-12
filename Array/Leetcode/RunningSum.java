f6f1e63dcbf016f0c77dece5670a35448a813843
Akram <akramshaik2910@gmail.com>
2026-08-12 14:22:11 +0530

    Add Running sum problem

---
+++ Array/Leetcode/RunningSum.java
@@ -0,0 +1,16 @@
+//leetcode 1480
+//Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).Return the running sum of nums.
+class Solution {
+    public int[] runningSum(int[] nums) {
+        for(int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+}
+
+public class RunningSum {
+    public static void main(String[] args) {
+        
+    }   
+}

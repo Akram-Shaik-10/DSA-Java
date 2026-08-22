4af85271ce0110e76fa9d7152502a1179e6c70f6
Akram <akramshaik2910@gmail.com>
2026-08-22 17:54:22 +0530

    Solve Ransom Note

---
+++ Hashing/Leetcode/RansomNote.java
@@ -0,0 +1,28 @@
+//leetcode 383
+import java.util.HashMap;
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character, Character> map = new HashMap<>();
+        HashMap<Character, Character> map2 = new HashMap<>();
+        
+
+        for(int i = 0; i < s.length(); i++) {
+            char a = s.charAt(i);
+            char b = t.charAt(i);
+            if(map.containsKey(a) && map.get(a) != b) {
+                return false;
+            }
+            if(map2.containsKey(b) && map2.get(b) != a) return false;
+            map.put(a, b);
+            map2.put(b, a);
+        }
+        return true;
+    }
+}
+
+public class RansomNote {
+    public static void main(String[] args) {
+        
+    }
+}
